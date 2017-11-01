3d837caaa69e404a87bbb7e1db36952a50b787be
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-11-01 23:47:02 +1300

    fade to greyscale on the click

--- src/background.js
+++ src/background.js
@@ -1,9 +1,11 @@
-
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' greyscale!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style="filter: grayscale(100%);"'
+  console.log("Turning " + tab.url + " greyscale!");
+  chrome.tabs.insertCSS(null, {
+    file: "./css/creatororconsumer.css"
   });
+  // chrome.tabs.executeScript({
+  //   code: 'document.body.style="filter: grayscale(100%);"'
+  // });
 });

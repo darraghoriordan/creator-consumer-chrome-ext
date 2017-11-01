4dd117c2cac6b84e1dbd9204006934f5ca314a84
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-11-02 00:14:42 +1300

    mucking about with tab listener

--- src/background.js
+++ src/background.js
@@ -1,11 +1,34 @@
+var weAreActive = false;
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log("Turning " + tab.url + " greyscale!");
-  chrome.tabs.insertCSS(null, {
-    file: "./css/creatororconsumer.css"
-  });
-  // chrome.tabs.executeScript({
-  //   code: 'document.body.style="filter: grayscale(100%);"'
-  // });
+  weAreActive = true;
+  setTimeout(turnOnGreyScaleTransitionAndTabListener, 3000);
 });
+
+function turnOnGreyScaleTransitionAndTabListener() {
+  turnOnGreyScaleTransition();
+  chrome.tabs.onCreated.addListener(function(tab) {
+    turnOnGreyScaleTransition();
+  });
+}
+function turnOnGreyScaleTransition() {
+  chrome.tabs.insertCSS(null, {
+    file: "./css/greyscale-timer.css"
+  });
+}
+
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get(
+    {
+      favoriteColor: "red",
+      likesColor: true
+    },
+    function(items) {
+      document.getElementById("color").value = items.favoriteColor;
+      document.getElementById("like").checked = items.likesColor;
+    }
+  );
+}
